3a2b099735bbac0d52b39a02a2ae286cced04420
CHARLINE <vergoz.charline@gmail.com>
2023-12-06 12:13:19 +0100

    create route delete

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const { getAllBooks, createBook, updateBook, getOneBook } = require('../controllers/books.js')
+const { getAllBooks, createBook, updateBook, getOneBook, deleteBook } = require('../controllers/books.js')
 
 router.get("/", getAllBooks);
 router.get("/:bookID", getOneBook);
 router.post("/", createBook)
 router.put("/:bookID", updateBook);
-
+router.delete("/:bookID", deleteBook);
 
 module.exports = router;
