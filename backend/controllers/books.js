2649ac046cafe339ca63c6897b6f0d130e4881ab
CHARLINE <vergoz.charline@gmail.com>
2023-12-06 12:01:59 +0100

    route post

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -6,6 +6,33 @@ async function getCollection() {
     return client.db("technicaltestzelin").collection("books");
   }
   
+  async function createBook(req, res) {
+    try {
+      const { title, author, date, note, rate, last_modification } = req.body;
+
+      if (!title || !author || !date || !note || !last_modification) {
+        return res.status(400).json({ message: "Please complete all fields" });
+      }
+  
+      const collection = await getCollection(); 
+      const result = await collection.insertOne({
+        title,
+        author,
+        date,
+        publication_date: new Date(),
+        note,
+        rate: Number(rate),
+        last_modification: new Date(),
+      });
+  
+      res.status(201).json({ message: 'Book create successfully' });
+    } catch (error) {
+      console.error("Error ", error);
+      res.status(500).json({ message: "Error when creating the book" });
+    }
+  }
+  
+  
 
 async function getAllBooks(req, res) {
     try {
@@ -18,5 +45,7 @@ async function getAllBooks(req, res) {
     }
   }
 
-  module.exports = { getAllBooks };
+  
+
+  module.exports = { getAllBooks, createBook };
   
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const { getAllBooks } = require('../controllers/books.js')
+const { getAllBooks, createBook } = require('../controllers/books.js')
 
 router.get("/", getAllBooks);
+router.post("/", createBook)
 
 
 module.exports = router;
